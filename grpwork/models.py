681347b2a6e93052b3679c605abafd702c1ad228
AbugaAroni <abugaaroni@gmail.com>
2020-10-28 13:49:46 +0300

    add neighbourhood model

--- grpwork/models.py
+++ grpwork/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     username = models.ForeignKey(User,on_delete=models.CASCADE)
-    Location = models.CharField(max_length=20)
+    Location = models.CharField(max_length=50)
     user_avatar = models.ImageField(upload_to = 'images/')
     Neighbourhood =  models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
 
@@ -20,3 +20,16 @@ class Profile(models.Model):
 
     class Meta:
         ordering = ['username']
+
+class Neighbourhood(models.Model):
+    Name = models.CharField(max_length=20)
+    Centres = models.models.ManyToManyField(Business_centres)
+
+    def __str__(self):
+        return self.name
+
+    def save_Neighbourhood(self):
+        self.save()
+
+    def delete_Neighbourhood(self):
+        self.delete()
