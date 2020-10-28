8b98d08f8e9d52eb9d1dedc97f53c6e58218fd8a
AbugaAroni <abugaaroni@gmail.com>
2020-10-28 14:11:32 +0300

    add working tests for business_centre and error fixes for application

--- grpwork/models.py
+++ grpwork/models.py
@@ -3,6 +3,33 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Business_centres(models.Model):
+    centre_name = models.CharField(max_length=20)
+    contact_info = models.CharField(max_length=20)
+    emergency_service = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.centre_name
+
+    def save_centre(self):
+        self.save()
+
+    def delete_centre(self):
+        self.delete()
+
+class Neighbourhood(models.Model):
+    Name = models.CharField(max_length=20)
+    Centres = models.ManyToManyField(Business_centres)
+
+    def __str__(self):
+        return self.name
+
+    def save_Neighbourhood(self):
+        self.save()
+
+    def delete_Neighbourhood(self):
+        self.delete()
+
 class Profile(models.Model):
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     Location = models.CharField(max_length=50)
@@ -20,30 +47,3 @@ class Profile(models.Model):
 
     class Meta:
         ordering = ['username']
-
-class Neighbourhood(models.Model):
-    Name = models.CharField(max_length=20)
-    Centres = models.models.ManyToManyField(Business_centres)
-
-    def __str__(self):
-        return self.name
-
-    def save_Neighbourhood(self):
-        self.save()
-
-    def delete_Neighbourhood(self):
-        self.delete()
-
-class Business_centres(model.Model):
-    centre_name = models.CharField(max_length=20)
-    contact_info = models.CharField(max_length=20)
-    emergency_service = models.booleanfield(default=false)
-
-    def __str__(self):
-        return self.centre_name
-
-    def save_centre(self):
-        self.save()
-
-    def delete_centre(self):
-        self.delete()
--- grpwork/tests.py
+++ grpwork/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Profile, Neighbourhood, Business_centres
+from django.contrib.auth.models import User
+
+# Create your tests here.
+class Business_centresClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.testcentre = Business_centres(centre_name="rick", contact_info="070707707", emergency_service=True)
+        self.testcentre.save()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.testcentre,Business_centres))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.testcentre.save_centre()
+        testsaved = Business_centres.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.testcentre.save_centre()
+        testsaved = Business_centres.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+        self.testcentre.delete_centre()
+        testdelete = Business_centres.objects.filter(centre_name="rick")
+        self.assertEqual(len(testdelete), 0)
 
 # Create your tests here.
