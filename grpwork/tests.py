f288ea176765e594f9dfe74641b76059320e4c65
AbugaAroni <abugaaroni@gmail.com>
2020-10-28 14:29:27 +0300

    add testing for profile class

--- grpwork/tests.py
+++ grpwork/tests.py
@@ -58,3 +58,40 @@ class NeighbourhoodClass(TestCase):
         self.testneighbourhood.delete_Neighbourhood()
         testdelete = Neighbourhood.objects.filter(name="Langata")
         self.assertEqual(len(testdelete), 0)
+
+class ProfileClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.testcentre = Business_centres(centre_name="rick", contact_info="070707707", emergency_service=True)
+        self.testcentre.save()
+
+        self.testneighbourhood = Neighbourhood(name="Langata")
+        self.testneighbourhood.save()
+        #add centre with many to many field
+        self.testneighbourhood.centres.add(self.testcentre)
+
+        self.testuser = User(username = "rick101", password="password")
+        self.testuser.save()
+
+        self.testprofile = Profile(username=self.testuser, location="Karen", user_avatar="wewweww",neighbourhood=self.testneighbourhood)
+        self.testprofile.save()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.testprofile,Profile))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.testprofile.save_profile()
+        testsaved = Profile.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+    # Testing Delete Method
+    def test_delete_method(self):
+        self.testprofile.save_profile()
+        testsaved = Profile.objects.all()
+        self.assertTrue(len(testsaved) > 0)
+
+        self.testprofile.delete_profile()
+        testdelete = Profile.objects.filter(username=self.testprofile.username)
+        self.assertEqual(len(testdelete), 0)
