e77ae9c5b5fd460d4a94ab157328547700a382fa
AbugaAroni <abugaaroni@gmail.com>
2020-10-28 13:46:15 +0300

    add profile model

--- grpwork/models.py
+++ grpwork/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    Location = models.CharField(max_length=20)
+    user_avatar = models.ImageField(upload_to = 'images/')
+    Neighbourhood =  models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['username']
