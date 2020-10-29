3cd28a905d046ad8c7dc967feb96c2198c74877c
AbugaAroni <abugaaroni@gmail.com>
2020-10-29 14:39:54 +0300

    will fix some errors and update the readme

--- groupwork/settings.py
+++ groupwork/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'grpwork',
+    'tinymce',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
